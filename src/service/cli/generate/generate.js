6695f74de9f911f0e2319a1cd37f83fb0a0c4458
kvezal <muckam49@mail.ru>
2020-04-04 11:51:20 +0800

    feat(generate): mock data moved to ./data (module2-task2)

--- src/service/cli/generate/generate.js
+++ src/service/cli/generate/generate.js
@@ -5,19 +5,12 @@ const dateFormat = require(`date-format`);
 const chalk = require(`chalk`);
 
 const {getNumbersDayInMilliseconds, getRandomInt, shuffle} = require(`../../utils`);
-const {
-  FILE_NAME,
-  TITLES,
-  TEXTS,
-  OLDEST_PUBLICATION_COUNT_DAY,
-  CATEGORIES,
-  Count,
-} = require(`./mock-params`);
+const params = require(`./params`);
 const {ExitCode} = require(`../../constants`);
 
 
 const currentDate = Number(new Date());
-const maxPublishedDaysAgoInMilliseconds = getNumbersDayInMilliseconds(OLDEST_PUBLICATION_COUNT_DAY);
+const maxPublishedDaysAgoInMilliseconds = getNumbersDayInMilliseconds(params.OLDEST_PUBLICATION_COUNT_DAY);
 
 const getDatePublication = () => {
   const publishedMillisecondsAgo = getRandomInt(0, maxPublishedDaysAgoInMilliseconds);
@@ -25,24 +18,34 @@ const getDatePublication = () => {
   return dateFormat(`yyyy-MM-dd hh:mm:ss`, date);
 };
 
-const generateArticles = (count) => (
+const generateArticles = (count, titles, texts, categories) => (
   Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
-    announce: TEXTS[getRandomInt(0, TEXTS.length - 1)],
-    fullText: shuffle(TEXTS).slice(1, getRandomInt(1, TEXTS.length - 1)).join(` `),
+    title: titles[getRandomInt(0, titles.length - 1)],
+    announce: texts[getRandomInt(0, texts.length - 1)],
+    fullText: shuffle(texts).slice(1, getRandomInt(1, texts.length - 1)).join(` `),
     createdDate: getDatePublication(),
-    category: shuffle(CATEGORIES).slice(1, getRandomInt(1, CATEGORIES.length - 1)),
+    category: shuffle(categories).slice(1, getRandomInt(1, categories.length - 1)),
   }))
 );
 
 const showErrorIfCountIsNotCorrect = (count) => {
-  if (count < Count.MIN) {
-    console.error(chalk.red(`Generating count can't be less then '${Count.MIN}'`));
+  if (count < params.Count.MIN) {
+    console.error(chalk.red(`Generating count can't be less then '${params.Count.MIN}'`));
     process.exit(ExitCode.ERROR);
   }
-  if (count > Count.MAX) {
-    console.error(chalk.red(`Generating count can't be more then '${Count.MAX}'`));
+  if (count > params.Count.MAX) {
+    console.error(chalk.red(`Generating count can't be more then '${params.Count.MAX}'`));
     process.exit(ExitCode.ERROR);
+  }
+};
+
+const readFile = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.trim().split(`\n`);
+  } catch (error) {
+    console.error(chalk.red(`Can't read file ${filePath}`));
+    return [];
   }
 };
 
@@ -50,11 +53,14 @@ module.exports = {
   name: `--generate`,
   alias: `-g`,
   async run(argv) {
-    const count = Number(argv) || Count.DEFAULT;
+    const count = Number(argv) || params.Count.DEFAULT;
     showErrorIfCountIsNotCorrect(count);
-    const content = JSON.stringify(generateArticles(count));
+    const TITLES = await readFile(params.FILE_TITLES_PATH);
+    const TEXTS = await readFile(params.FILE_TEXTS_PATH);
+    const CATEGORIES = await readFile(params.FILE_CATEGORIES_PATH);
+    const content = JSON.stringify(generateArticles(count, TITLES, TEXTS, CATEGORIES));
     try {
-      await fs.writeFile(FILE_NAME, content);
+      await fs.writeFile(params.FILE_NAME, content);
       console.info(chalk.green(`Operation success. File created.`));
     } catch (error) {
       console.error(chalk.red(`Can't write data to file...`));
