f3cae230d3ef25645c0af2fb93000bc20a82b7ff
kvezal <muckam49@mail.ru>
2020-05-21 14:20:24 +0800

    feat(package): test::backend::dev to test script (module4-task2)

--- webpack/backend.config.js
+++ webpack/backend.config.js
@@ -4,7 +4,6 @@ const path = require(`path`);
 
 const nodeExternals = require(`webpack-node-externals`);
 const NodemonPlugin = require(`nodemon-webpack-plugin`);
-const CopyPlugin = require(`copy-webpack-plugin`);
 const {CleanWebpackPlugin} = require(`clean-webpack-plugin`);
 
 
