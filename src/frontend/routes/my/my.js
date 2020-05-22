37899ee33f24e80d21425529a5f7eaaea36e3b5b
kvezal <muckam49@mail.ru>
2020-05-22 15:09:11 +0800

    feat(frontend): my page content desplayed is ready (module4-task3)

--- src/frontend/routes/my/my.js
+++ src/frontend/routes/my/my.js
@@ -1,37 +1,14 @@
 'use strict';
 
 const {Router} = require(`express`);
-const {logger} = require(`frontend/utils`);
-const {dateAdapter} = require(`frontend/adapters`);
 
 const commentsRoute = require(`./comments`);
+const {getMyPage} = require(`./methods`);
 
 
 const myRoute = new Router();
 
 myRoute.use(`/comments`, commentsRoute);
-myRoute.get(`/`, (req, res) => {
-  const content = {
-    articleList: [
-      {
-        id: 1,
-        date: dateAdapter.get(`2019-03-21T20:33`),
-        title: `AirPods в один клик`,
-      },
-      {
-        id: 2,
-        date: dateAdapter.get(`2019-03-21T20:33`),
-        title: `AirPods в один клик`,
-      },
-      {
-        id: 3,
-        date: dateAdapter.get(`2019-03-21T20:33`),
-        title: `AirPods в один клик`,
-      },
-    ],
-  };
-  res.render(`pages/my/my`, content);
-  logger.endRequest(req, res);
-});
+myRoute.get(`/`, getMyPage);
 
 module.exports = myRoute;
